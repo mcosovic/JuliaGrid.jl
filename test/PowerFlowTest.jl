e080cc85f0782c4e92182461a68dea52642f0554
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2020-04-13 21:04:18 +0200

    add cases

--- test/PowerFlowTest.jl
+++ test/PowerFlowTest.jl
@@ -4,8 +4,8 @@ using JuliaGrid
 using HDF5
 using Test
 
-testData = "PowerFlowTest_case30modified.h5"
-data = "case30modified.h5"
+testData = "PowerFlowTest_case30.h5"
+data = "case30.h5"
 accuracy = 1e-7
 path = abspath(joinpath(dirname(Base.find_package("JuliaGrid")), ".."), "test/", testData)
 
