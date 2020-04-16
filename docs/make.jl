218f3616e934c335bb2b2e31a04400013dd03d6e
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2020-04-16 17:57:55 +0200

    try

--- docs/make.jl
+++ docs/make.jl
@@ -3,9 +3,6 @@ using Documenter, JuliaGrid
 makedocs(
     sitename = "JuliaGrid",
     modules = [JuliaGrid],
-    format = Documenter.HTML(
-        highlights = ["yaml"],
-    ),
     clean = false,
     doctest = false,
     pages = [
