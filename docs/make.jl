413f1b00d6b58659a7fe83f121efa394934c3342
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2020-04-16 17:36:05 +0200

    new

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,10 @@ using Documenter, JuliaGrid
 makedocs(
     sitename = "JuliaGrid",
     modules = [JuliaGrid],
+    format = Documenter.HTML(
+        highlights = ["yaml"],
+    ),
+    clean = false,
     doctest = false,
     pages = [
         "Home" => "index.md",
