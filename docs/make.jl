85794f981930df03553e1f5214a7de2356afbe73
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2020-04-16 14:39:12 +0200

    Update make.jl
    na

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, JuliaGrid
 makedocs(
     sitename = "JuliaGrid",
     modules = [ JuliaGrid ],
+    doctest = false, 
     pages = [
         "Home" => "index.md",
         "Power Flow" => "man/flow.md"
