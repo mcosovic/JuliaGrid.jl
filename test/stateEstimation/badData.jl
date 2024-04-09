a8a0cb03fbdd90f48e85ca15791f7df5a154d152
mcosovic <mirsad.cosovic@gmail.com>
2024-04-09 13:42:51 +0200

    add bad data tests

--- test/stateEstimation/badData.jl
+++ test/stateEstimation/badData.jl
@@ -1,6 +1,171 @@
 system14 = powerSystem(string(pathData, "case14test.m"))
 system30 = powerSystem(string(pathData, "case30test.m"))
 
+@testset "AC State Estimation: Bad Data" begin
+    @default(template)
+    @default(unit)
+
+    ############### Modified IEEE 14-bus Test Case ################
+    updateBus!(system14; label = 1, type = 2)
+    updateBus!(system14; label = 3, type = 3, angle = -0.17)
+
+    acModel!(system14)
+    analysis = newtonRaphson(system14)
+    for i = 1:1000
+        stopping = mismatch!(system14, analysis)
+        if all(stopping .< 1e-12)
+            break
+        end
+        solve!(system14, analysis)
+    end
+    power!(system14, analysis)
+    current!(system14, analysis)
+
+    device = measurement()
+    @varmeter(label = "Varmeter ?")
+    for (key, value) in system14.bus.label
+        addVoltmeter!(system14, device; bus = key, magnitude = analysis.voltage.magnitude[value], noise = false)
+        addWattmeter!(system14, device; bus = key, active = analysis.power.injection.active[value], noise = false)
+        addVarmeter!(system14, device; bus = key, reactive = analysis.power.injection.reactive[value], noise = false)
+    end
+
+    for (key, value) in system14.branch.label
+        addWattmeter!(system14, device; from = key, active = analysis.power.from.active[value], noise = false)
+        addWattmeter!(system14, device; to = key, active = analysis.power.to.active[value], noise = false)
+        addVarmeter!(system14, device; from = key, reactive = analysis.power.from.reactive[value], noise = false)
+        addVarmeter!(system14, device; to = key, reactive = analysis.power.to.reactive[value], noise = false)
+    end
+
+    ####### WLS LU: One Outlier #######
+    updateVarmeter!(system14, device; label = "Varmeter 4", reactive = 10.25, noise = false)
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "Varmeter 4"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 52.5 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    @test analysis.voltage.magnitude ≈ analysisSE.voltage.magnitude
+    @test analysis.voltage.angle ≈ analysisSE.voltage.angle
+
+
+    ###### WLS LU: Two Outliers #######
+    @pmu(label = "PMU ?")
+    for (key, value) in system14.bus.label
+        addPmu!(system14, device; bus = key, magnitude = analysis.voltage.magnitude[value], angle = analysis.voltage.angle[value], noise = false)
+    end
+
+    updateVarmeter!(system14, device; label = "Varmeter 4", status = 1)
+    updatePmu!(system14, device; label = "PMU 10", magnitude = 30, noise = false)
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "PMU 10"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 7713.26 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "Varmeter 4"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 78.3 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    @test analysis.voltage.magnitude ≈ analysisSE.voltage.magnitude
+    @test analysis.voltage.angle ≈ analysisSE.voltage.angle
+
+    ####### WLS Orthogonal: Two Outliers #######
+    updateVarmeter!(system14, device; label = "Varmeter 4", status = 1)
+    updatePmu!(system14, device; label = "PMU 10", statusMagnitude = 1)
+
+    analysisSE = gaussNewton(system14, device, Orthogonal)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "PMU 10"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 7713.26 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device, Orthogonal)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "Varmeter 4"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 78.3 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device, Orthogonal)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    @test analysis.voltage.magnitude ≈ analysisSE.voltage.magnitude
+    @test analysis.voltage.angle ≈ analysisSE.voltage.angle
+
+
+    ####### PMU Rectangular #######
+    for (key, value) in system14.branch.label
+        addPmu!(system14, device; from = key, magnitude = analysis.current.from.magnitude[value], angle = analysis.current.from.angle[value], noise = false, polar = false)
+    end
+    updatePmu!(system14, device; label = "PMU 20", magnitude = 30)
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    residualTest!(system14, device, analysisSE; threshold = 3.0)
+    @test analysisSE.method.outlier.label == "PMU 20"
+    @test analysisSE.method.outlier.maxNormalizedResidual ≈ 335.59 atol = 1e-1
+
+    analysisSE = gaussNewton(system14, device)
+    for iteration = 1:20
+        stopping = solve!(system14, analysisSE)
+        if stopping < 1e-8
+            break
+        end
+    end
+    @test analysis.voltage.magnitude ≈ analysisSE.voltage.magnitude
+    @test analysis.voltage.angle ≈ analysisSE.voltage.angle
+end
 
 @testset "PMU State Estimation: Bad Data" begin
     @default(template)
