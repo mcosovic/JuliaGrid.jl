55671b331ba738ac9a10b038b24f7851611774d2
Documenter.jl <documenter@juliadocs.github.io>
2025-02-04 10:29:43 +0000

    build based on 0ae6df9

--- dev/examples/analyses/dcPowerFlow.jl
+++ dev/examples/analyses/dcPowerFlow.jl
@@ -9,19 +9,18 @@ using JuliaGrid
 system = powerSystem()
 
 addBus!(system; label = "Bus 1", type = 3, angle = 0.0)
-addBus!(system; label = "Bus 2", conductance = 0.1)
-addBus!(system; label = "Bus 3", active = 20.2)
+addBus!(system; label = "Bus 2", active = 20.2)
+addBus!(system; label = "Bus 3", conductance = 0.1)
 addBus!(system; label = "Bus 4", active = 50.8)
 
-@branch(label = "Branch ?", reactance = 0.22)
-addBranch!(system; from = "Bus 1", to = "Bus 2")
-addBranch!(system; from = "Bus 1", to = "Bus 3")
-addBranch!(system; from = "Bus 2", to = "Bus 3")
-addBranch!(system; from = "Bus 2", to = "Bus 4", shiftAngle = -2.3)
+@branch(reactance = 0.22)
+addBranch!(system; label = "Branch 1", from = "Bus 1", to = "Bus 3")
+addBranch!(system; label = "Branch 2", from = "Bus 1", to = "Bus 2")
+addBranch!(system; label = "Branch 3", from = "Bus 2", to = "Bus 3")
+addBranch!(system; label = "Branch 4", from = "Bus 3", to = "Bus 4", shiftAngle = -2.3)
 
-@generator(label = "Generator ?")
-addGenerator!(system; bus = "Bus 1", active = 60.1)
-addGenerator!(system; bus = "Bus 3", active = 18.2)
+addGenerator!(system; label = "Generator 1", bus = "Bus 1", active = 60.1)
+addGenerator!(system; label = "Generator 2", bus = "Bus 2", active = 18.2)
 
 dcModel!(system)
 
@@ -36,11 +35,11 @@ printBranchData(system, analysis)
 
 
 ##### Modifying Generators and Demands #####
-updateBus!(system, analysis; label = "Bus 3", active = 25.5)
+updateBus!(system, analysis; label = "Bus 2", active = 25.5)
 updateBus!(system, analysis; label = "Bus 4", active = 42.0)
 
 updateGenerator!(system, analysis; label = "Generator 1", active = 58.0)
-updateGenerator!(system, analysis; label = "Generator 2", active = 23.1)
+updateGenerator!(system, analysis; label = "Generator 2", active = 23.0)
 
 solve!(system, analysis)
 power!(system, analysis)
