a0cafb43501c0a1348fe90cbe1e1f4b6ec607c13
mcosovic <mirsad.cosovic@gmail.com>
2025-03-11 10:30:28 +0100

    fixed Power Generation display in Bus Summary

--- src/print/powerSystem.jl
+++ src/print/powerSystem.jl
@@ -1391,7 +1391,7 @@ function busSummary(
         eval(smr, i, scaleVolg, voltage.magnitude[i], "Voltage Magnitude")
         eval(smr, i, scale[:θ], voltage.angle[i], "Voltage Angle")
 
-        if !haskey(bus.supply.generator, i)
+        if haskey(bus.supply.generator, i)
             if haskey(smr.type, "Power Generation")
                 smr.inuse["Power Generation"] += 1
                 eval(smr, i, scale[:P], power.supply.active[i], "Power Generation Active")
